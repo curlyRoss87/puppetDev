2659527a98b9b16d9343ce17d215457164ebe7ad
root <root@pre-devpjr-bld01.home.sys.ross.ic.gov.uk>
2018-07-29 13:33:01 +0100

    Adding the rectifier class

--- init.pp
+++ init.pp
@@ -1,2 +1,9 @@
 # init pp file for puppet dev
 
+# This will create a file in /home/rcleac1 with a speciied content
+
+class rectifier {
+	file {'/home/rcleac1/test':
+	content => "This is a test file"
+	}
+}

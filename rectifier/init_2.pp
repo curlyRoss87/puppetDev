f4d45e2208360f930d9626211caa8c91da4072c0
root <root@pre-devpjr-bld01.home.sys.ross.ic.gov.uk>
2018-08-02 21:53:45 +0100

    addition of init_2.pp for dev purposes

---
+++ rectifier/init_2.pp
@@ -0,0 +1,44 @@
+# Init pp for product install 
+
+class rectifier (
+
+# Define the Variables needed in the class structure
+
+	hardware	type = undef, 	# Hardware to install on
+	$subType	type = undef,	# Lowside or highside
+	$mgmtIf		type = undef,	# Management interface
+	$ddsIf		type = undef,	# Data interface
+	$ddsNetWk	type = undef,	# Data Network Address
+	$ddsGW		type = undef,	# Data Network Gateway
+	$ddsMask	type = undef,	# Data NetMask in CIDR Notation i.e. 24 for /24 or 255.255.255.0
+	$version	type = undef,	# Version to install, explicit or Latest
+
+)
+{
+ 	case $subType {
+		lowside { notify ("You are installing the lowside")
+		}
+     		highside { notify ("You are installing the highside")
+		}
+	}
+
+}
+
+class rectifier:lowside {
+	file_line { 'rectifier_type = $subType':
+	match => '^rectifier_type',
+	notify =>  Service['diode.target'],
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+

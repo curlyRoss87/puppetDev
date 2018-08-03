d1b5566db6fb01f91257a0c84d870d10a49c073e
root <root@pre-devpjr-bld01.home.sys.ross.ic.gov.uk>
2018-08-03 20:53:15 +0100

    rename init_2 to init.pp

--- rectifier/init.pp
+++ rectifier/init.pp
@@ -1,8 +1,57 @@
-# init pp file for puppet dev
+# Init pp for product install 
 
-# This will create a file in /home/rcleac1 with a speciied content
+class rectifier (
 
-class rectifier {
-	file {'/home/rcleac1/test':
-	content => "This is a test file"
-	}}
+# Define the Variables needed in the class structure
+
+	hardware	type = undef, 	# Hardware to install on
+	$subType	type = undef,	# Lowside or highside
+	$mgmtIf		type = undef,	# Management interface
+	$ddsIf		type = undef,	# Data interface
+	$ddsNetWk	type = undef,	# Data Network Address
+	$ddsGW		type = undef,	# Data Network Gateway
+	$ddsMask	type = undef,	# Data NetMask in CIDR Notation i.e. 24 for /24 or 255.255.255.0
+	$version	type = undef,	# Version to install, explicit or Latest
+
+)
+{
+	service {'diode.target':
+	name	=> 'diode.target',
+	ensure	=> running,
+	enable	=> true,
+	}
+
+ 	case $component	 {
+		'lowside' {
+	class rectifier::lowside::p_install.pp {
+		require 	=> version['$version'],
+		notify		=> Service['diode.target'],
+			
+		
+
+		}
+	}
+     		highside { notify ("You are installing the highside")
+		}
+	}
+
+}
+
+class rectifier:lowside {
+	file_line { 'rectifier_type = $subType':
+	match => '^rectifier_type',
+	notify =>  Service['diode.target'],
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
